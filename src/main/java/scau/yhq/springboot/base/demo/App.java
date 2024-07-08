7349c1d2ba92c44f2933b90cf0902690f146ec9f
mabeining <mabeining@aspirecn.com>
2024-07-08 21:14:05 +0800

    refactor: 移除spring-cloud-starter-bootstrap依赖

--- src/main/java/scau/yhq/springboot/base/demo/App.java
+++ src/main/java/scau/yhq/springboot/base/demo/App.java
@@ -1,15 +1,27 @@
 package scau.yhq.springboot.base.demo;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
 /**
  * @author yanhaoqiang
  * @date 2022-03-28
  */
-@SpringBootApplication
-@ComponentScan({"scau.yhq.springboot.base.demo.*"})
+@SpringBootApplication(scanBasePackages = {"scau.yhq.springboot.base.demo.*"})
 public class App {
+    private final Logger log = LoggerFactory.getLogger(App.class);
+
+    @Value("${foo.bar}")
+    private String value;
+
+    @PostConstruct
+    public void init() {
+        log.info("value from nacos: {}", value);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
